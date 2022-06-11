f343a52aff31d09ea7ce0030a61151d627428fa9
marty1885 <marty188586@gmail.com>
2022-06-11 18:25:30 +0800

    fix link rewrite

--- tlgs/server/main.cpp
+++ tlgs/server/main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char** argv)
             while(std::getline(ss, line)) {
                 if(line.starts_with("=>") && 
                     (line.find("/search") != std::string::npos || line.find("/backlinks") != std::string::npos
-                    || line.find("/add_seed") != std::string::npos)) {
+                    || line.find("/add_seed") != std::string::npos) && line.find("/doc") == std::string::npos) {
                     utils::replaceAll(line, "=>", "=:");
                 }
                 result += line + "\n";
