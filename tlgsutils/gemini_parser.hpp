b43fdf9f7effff094767169867ecaaf9879225fa
marty1885 <marty188586@gmail.com>
2022-06-11 13:07:20 +0800

    add document for extractGemini(Cnocise)

--- tlgsutils/gemini_parser.hpp
+++ tlgsutils/gemini_parser.hpp
@@ -14,6 +14,19 @@ struct GeminiDocument
     std::vector<std::string> links;
 };
 
+/**
+ * @brief Parse and convert a Gemini document into title
+ *  , plaintext(without styling and heading) and links
+ * 
+ * @param sv the Gemini document
+ */
 GeminiDocument extractGemini(const std::string_view sv);
+
+/**
+ * @brief Parse and convert a Gemini document into title
+ * , plaintext(without styling and heading and ASCII art) and links
+ * 
+ * @param sv the Gemini document
+ */
 GeminiDocument extractGeminiConcise(const std::string_view sv);
 }
